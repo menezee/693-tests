e0644d59ca985c32da82507ff47b749df3b57325
Erich Menezes <erich.menezes1@gmail.com>
2022-05-16 14:59:00 -0300

    :card_file_box: db authn changes

--- grocery-list/backend/src/database/index.js
+++ grocery-list/backend/src/database/index.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 async function connect() {
-  await mongoose.connect('mongodb://balta:e296cd9f@localhost:27017');
+  await mongoose.connect('mongodb://user:password@localhost:27017');
 }
 
 export default {
